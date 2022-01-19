da01d6b9938327a87413752e870f0d08c2f729c3
Ezequiel Parziale <eparziale@gmail.com>
2022-01-19 20:11:52 -0300

    :star: feature: reaction listener

---
+++ app/listeners/reaction.py
@@ -0,0 +1,9 @@
+from flask import Flask
+from slack_bolt import App
+
+
+def register_listener(app: App, flask_app: Flask):
+
+    @app.event("reaction_added")
+    def reaction_message(say):
+        say("Hey! Gracias!")
