9e1af7e78dfca5dcb7974bddfb20cbeeb70097b4
Ezequiel Parziale <eparziale@gmail.com>
2022-01-19 20:08:20 -0300

    :star: feature: funcion del clima

---
+++ app/listeners/weather.py
@@ -0,0 +1,17 @@
+from flask import Flask
+from slack_bolt import App
+from message_blocks import MessageBlocks
+from weather import Weather
+
+def register_listener(app: App, flask_app: Flask):
+
+    @app.command("/clima")
+    def slash_get_clima(ack, body, client):
+        ack()
+
+        text = body.get("text")
+        clima = Weather.get_clima(text)
+        block_message = MessageBlocks.get_weather_block(clima)
+        user_id = body.get("user_id")
+
+        client.chat_postMessage(text=f"Clima", blocks=block_message, channel=user_id)
