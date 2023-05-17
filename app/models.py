057229960f913a56e1a04640c5cabd7d0c24ec44
Ezequiel Parziale <eparziale@gmail.com>
2023-05-17 20:56:50 -0300

    :card_file_box: update: upgrade to sqlalchemy 2

--- app/models.py
+++ app/models.py
@@ -1,4 +1,7 @@
-from sqlalchemy import BOOLEAN, FLOAT, Column, Integer, String
+from datetime import datetime
+
+from sqlalchemy import BOOLEAN, FLOAT, Integer, String
+from sqlalchemy.orm import Mapped, mapped_column
 from sqlalchemy.sql.expression import text
 from sqlalchemy.sql.sqltypes import TIMESTAMP, DateTime
 
@@ -8,26 +11,26 @@ from app import db
 class Message(db.Model):
     __tablename__ = "messages"
 
-    id = Column(Integer, primary_key=True, nullable=False)
-    user = Column(String(80), nullable=False)
-    message = Column(String(120), nullable=False)
-    channel = Column(String(120), nullable=False)
-    channel_type = Column(String(120), nullable=False)
-    ts = Column(FLOAT, nullable=False)
-    date = Column(DateTime, nullable=False)
-    thread = Column(BOOLEAN, nullable=False)
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, nullable=False)
+    user: Mapped[str] = mapped_column(String(80), nullable=False)
+    message: Mapped[str] = mapped_column(String(120), nullable=False)
+    channel: Mapped[str] = mapped_column(String(120), nullable=False)
+    channel_type: Mapped[str] = mapped_column(String(120), nullable=False)
+    ts: Mapped[float] = mapped_column(FLOAT, nullable=False)
+    date: Mapped[datetime] = mapped_column(DateTime, nullable=False)
+    thread: Mapped[bool] = mapped_column(BOOLEAN, nullable=False)
 
 
 class User(db.Model):
     __tablename__ = "users"
 
-    id = Column(String(80), nullable=False, primary_key=True)
-    user = Column(String(80), nullable=False)
-    channel = Column(String(120), nullable=False)
-    created_at = Column(
+    id: Mapped[int] = mapped_column(String(80), nullable=False, primary_key=True)
+    user: Mapped[str] = mapped_column(String(80), nullable=False)
+    channel: Mapped[str] = mapped_column(String(120), nullable=False)
+    created_at: Mapped[datetime] = mapped_column(
         TIMESTAMP(timezone=True), server_default=text("now()"), nullable=False
     )
-    last_message = Column(
+    last_message: Mapped[datetime] = mapped_column(
         TIMESTAMP(timezone=True), server_default=text("now()"), nullable=False
     )
 
@@ -35,8 +38,8 @@ class User(db.Model):
 class Scheduled(db.Model):
     __tablename__ = "scheduled_messages"
 
-    id = Column(Integer, primary_key=True, nullable=False)
-    scheduled_message_id = Column(String(120), nullable=False)
-    channel = Column(String(120), nullable=False)
-    text = Column(String(120), nullable=False)
-    post_at = Column(FLOAT, nullable=False)
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, nullable=False)
+    scheduled_message_id: Mapped[str] = mapped_column(String(120), nullable=False)
+    channel: Mapped[str] = mapped_column(String(120), nullable=False)
+    text: Mapped[str] = mapped_column(String(120), nullable=False)
+    post_at: Mapped[float] = mapped_column(FLOAT, nullable=False)
