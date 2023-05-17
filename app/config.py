568407b3d3a8f40aac47dcc89e23ac4578517bc3
Ezequiel Parziale <eparziale@gmail.com>
2023-05-17 20:54:07 -0300

    :wrench: update: add new env vars

--- app/config.py
+++ app/config.py
@@ -1,13 +1,42 @@
-from pydantic import BaseSettings
+from typing import Any, Dict, Optional
+
+from pydantic import BaseSettings, PostgresDsn, validator
 
 
 class Settings(BaseSettings):
+    # Flask
     SECRET_KEY: str
     SQLALCHEMY_TRACK_MODIFICATIONS: bool = False
+    SQLALCHEMY_ECHO: bool = False
+
+    # Slack
     SLACK_BOT_TOKEN: str
     SLACK_SIGNING_SECRET: str
+
+    # External API
     WEATHER_TOKEN: str
-    SQLALCHEMY_DATABASE_URI: str
+
+    # Database
+    POSTGRES_HOSTNAME: str
+    POSTGRES_PORT: str
+    POSTGRES_USER: str
+    POSTGRES_PASSWORD: str
+    POSTGRES_DB: str
+
+    SQLALCHEMY_DATABASE_URI: Optional[PostgresDsn] = None
+
+    @validator("SQLALCHEMY_DATABASE_URI", pre=True)
+    def assemble_db_connection(cls, v: Optional[str], values: Dict[str, Any]) -> Any:
+        if isinstance(v, str):
+            return v
+        return PostgresDsn.build(
+            scheme="postgresql",
+            user=values.get("POSTGRES_USER"),
+            password=values.get("POSTGRES_PASSWORD"),
+            host=values.get("POSTGRES_HOSTNAME"),
+            port=values.get("POSTGRES_PORT"),
+            path=f"/{values.get('POSTGRES_DB') or ''}",
+        )
 
     class Config:
         env_file = ".env"
